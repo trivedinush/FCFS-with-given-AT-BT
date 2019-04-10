d4b72e1a92a58969d5c60ae303957c1824bb2392
trivedinush <46523033+trivedinush@users.noreply.github.com>
2019-04-10 16:27:23 +0530

    Add files via upload

---
+++ ImplementingRunnable.java
@@ -0,0 +1,43 @@
+class ThisThread implements Runnable
+
+{ 
+  public void run()
+	
+	{
+    
+		for(int i=0;i<10;i++)
+		
+		{
+      
+			System.out.println("Child Thread");
+    
+		}	  
+  
+	}
+
+}
+
+class ThreadDemo
+
+{
+	public static void main(String arg[])
+	
+	{
+		
+		ThisThread t=new ThisThread();
+		
+		Thread t1=new Thread(t);
+		
+		t1.start();
+		
+		for(int i=0;i<10;i++)
+		
+		{
+      
+			System.out.println("Main Thread");
+    
+		}
+	
+	}
+
+}
