636da529a0d8208720b6b8c7c1ef8994078f264e
trivedinush <46523033+trivedinush@users.noreply.github.com>
2019-04-10 16:31:25 +0530

    Add files via upload

---
+++ ExtendingThread.java
@@ -0,0 +1,40 @@
+class ThisThread extends Thread
+{ 
+  public void run()
+	
+	{
+    
+		for(int i=0;i<10;i++)
+		
+		{
+      
+			System.out.println("Child Thread");
+    
+		}	  
+  
+	}
+
+}
+
+class ThreadDemo
+
+{
+	public static void main(String arg[])
+	
+	{
+		
+		ThisThread t=new ThisThread();
+				
+		t.start();
+		
+		for(int i=0;i<10;i++)
+		
+		{
+      
+			System.out.println("Main Thread");
+    
+		}
+	
+	}
+
+}
